7d88446a61611a4bed6bf0f674b0ac615e13ad98
Noah <ntidwell34@yahoo.com>
2022-07-07 13:54:39 -0500

    array contract

---
+++ ArrayContract.sol
@@ -0,0 +1,19 @@
+// Use Remix to compile and deploy
+pragma solidity ^0.6.0;
+
+contract ArrayContract {
+    // Array
+    uint[] public uintArray = [1,2,3];
+
+    string[] public stringArray = ["apple", "banana", "grape"];
+    string[] public values;
+    uint[][] public array2D = [[1,2,3], [4,5,6]];
+
+    function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+    function valueCount() public view returns(uint) {
+        return values.length;
+    }
+}
